73f4dc8f395ecdbda7e1e220033450a13a1a2c6b
강우진 <spacein1001@gmail.com>
2026-03-11 16:01:00 +0900

    Update application.yaml

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -6,12 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // 예전 main에서는 객체생성, 의존성연결, 테스트실행 모두 담당
 // AppConfig - 객체생성 및 연결
 // TestDataRunner - 테스트
-// DisCodeitApplication - 앱 시작
+// DiscodeitApplication - 앱 시작
 @SpringBootApplication
 public class DiscodeitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiscodeitApplication.class, args);
 	}
-
 }
--- src/main/java/com/sprint/mission/discodeit/config/AppConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/AppConfig.java
@@ -9,9 +9,9 @@ import com.sprint.mission.discodeit.repository.file.FileUserRepository;
 import com.sprint.mission.discodeit.service.ChannelService;
 import com.sprint.mission.discodeit.service.MessageService;
 import com.sprint.mission.discodeit.service.UserService;
-import com.sprint.mission.discodeit.service.file.FileChannelService;
-import com.sprint.mission.discodeit.service.file.FileMessageService;
-import com.sprint.mission.discodeit.service.file.FileUserService;
+import com.sprint.mission.discodeit.service.basic.BasicChannelService;
+import com.sprint.mission.discodeit.service.basic.BasicMessageService;
+import com.sprint.mission.discodeit.service.basic.BasicUserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -42,20 +42,20 @@ public class AppConfig {
 
     @Bean
     public UserService userService(UserRepository userRepository) {
-        return new FileUserService(userRepository);
+        return new BasicUserService(userRepository);
     }
 
     @Bean
     public ChannelService channelService(ChannelRepository channelRepository) {
-        return new FileChannelService(channelRepository);
+        return new BasicChannelService(channelRepository);
     }
 
     @Bean
     public MessageService messageService(
             MessageRepository messageRepository,
-            UserService userService,
-            ChannelService channelService
+            UserRepository userRepository,
+            ChannelRepository channelRepository
     ) {
-        return new FileMessageService(messageRepository, userService, channelService);
+        return new BasicMessageService(messageRepository, userRepository, channelRepository);
     }
 }
