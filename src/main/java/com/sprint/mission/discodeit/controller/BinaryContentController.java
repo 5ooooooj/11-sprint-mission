d7824e41ba450a9550d6c3e25c14a81dc21b4c81
강우진 <spacein1001@gmail.com>
2026-03-25 10:06:58 +0900

    Implement BinaryContentController

--- src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -1,4 +1,42 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.dto.BinaryContentCreateRequest;
+import com.sprint.mission.discodeit.dto.BinaryContentResponse;
+import com.sprint.mission.discodeit.service.BinaryContentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/api/binary-contents")
+@RequiredArgsConstructor
 public class BinaryContentController {
+    private final BinaryContentService binaryContentService;
+
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.POST)
+    public BinaryContentResponse create(@RequestBody BinaryContentCreateRequest request){
+        return binaryContentService.create(request);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/{binaryContentId}", method = RequestMethod.GET)
+    public BinaryContentResponse findById(@PathVariable UUID binaryContentId){
+        return binaryContentService.findById(binaryContentId);
+    }
+
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.GET)
+    public List<BinaryContentResponse> findAllByIdIn(@RequestParam List<UUID> ids){
+        return binaryContentService.findAllByIdIn(ids);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/{binaryContentId}")
+    public void delete(@PathVariable UUID binaryContentId){
+        binaryContentService.delete(binaryContentId);
+    }
 }
